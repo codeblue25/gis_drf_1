cfb338f922d2eea7600d714b048c057b3044b8dd
codeblue25 <chois1031@gmail.com>
2021-12-14 10:06:49 +0900

    Account Update View update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,17 +1,20 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
+    # 메인 화면
     path('hello_world/', hello_world, name='hello_world'),
 
+    # 로그인, 로그아웃
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
+    # CRUD 패턴
     path('create/', AccountCreateView.as_view(), name='create'),
-
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 ]
